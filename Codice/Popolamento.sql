a5fb953b0c0be8896478b481a46c60016bc376c3
Pasquale Ruotolo <77306367+Just-Ipavon@users.noreply.github.com>
2022-07-18 12:40:21 +0200

    si

--- Codice/Popolamento.sql
+++ Codice/Popolamento.sql
@@ -1,7 +1,7 @@
 INSERT INTO Tipo_Uva(Specie,Glucosio,Colore,Costo_Impianto) VALUES('fragola',10,'Rosso',1000);
 INSERT INTO Tipo_Uva(Specie,Glucosio,Colore,Costo_Impianto) VALUES('zibibbo',9,'bianco',700);
 
-insert into Tipo_Vino(Nome_Vino,Acidita,Colore,Gradazione,Temp_Servizio,Tannini,Zuccheri_NF) values('chianti',4,'rosso',15,22,1,2);
+INSERT INTO Tipo_Vino(Nome_Vino,Acidita,Colore,Gradazione,Temp_Servizio,Tannini,Zuccheri_NF) values('chianti',4,'rosso',15,22,1,2);
 
 INSERT INTO Composizione_Vino(Nome_Vino,Specie,Percentuale) VALUES('chianti','zibibbo',30);
 INSERT INTO Composizione_Vino(Nome_Vino,Specie,Percentuale) VALUES('chianti','fragola',70);
