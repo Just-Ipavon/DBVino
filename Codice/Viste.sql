2730d9f482d37610147c5997c9ae4389df3d1a9a
BadBro00 <82472181+BadBro00@users.noreply.github.com>
2022-07-20 11:52:31 +0200

    Controlla vista Vini_Certificati

--- Codice/Viste.sql
+++ Codice/Viste.sql
@@ -32,3 +32,19 @@ CREATE OR REPLACE VIEW CONF AS
     #TIPO_VINO CLIENTE fatta
     #CONFEZIONE CLIENTE fatta
     #
+
+CREATE VIEW Vini_Certificati AS(
+SELECT (Tipo_Vino.Nome_Vino,Tipo_Vino.Gradazione,Tipo_Vino.Colore,Tipo_Vino.Temp_Servizio) FROM Tipo_Vino
+UNION
+SELECT (Nome_Imbott) FROM Imbottigliatore
+WHERE(
+    Controllo_Certificazione.Esito = 1 AND Tipo_Vino.Nome_Vino = Controllo_Certificazione.Nome_Vino
+    )
+)
+
+CREATE VIEW Vini_Certificati AS(
+SELECT (Nome_Vino,Gradazione,Colore,Temp_Servizio) FROM Tipo_Vino
+LEFT JOIN ON
+SELECT (Esito) FROM Controllo_Certificazione
+WHERE (Tipo_Vino.Nome_Vino = Controllo_Certificazione.Nome_Vino) AND (Controllo_Certificazione.Esito = 1)
+);
