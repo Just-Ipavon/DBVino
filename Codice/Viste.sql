9341d32f3574219db42babef36c17f7667cb1b69
BadBro00 <82472181+BadBro00@users.noreply.github.com>
2022-07-20 10:04:29 +0200

    Viste aggiornate

--- Codice/Viste.sql
+++ Codice/Viste.sql
@@ -1,24 +1,26 @@
-CREATE OR REPLACE VIEW Vini_DOC AS
-    SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
-    FROM Controllo_Certificazione
-    WHERE Certificato_Richiesto = 'DOC' && Esito = 1;
 
-CREATE OR REPLACE VIEW Vini_DOP AS
-    SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
-    FROM Controllo_Certificazione
-    WHERE Certificato_Richiesto = 'DOP' && Esito = 1;
-
-CREATE OR REPLACE VIEW Vini_DOCG AS
-    SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
-    FROM Controllo_Certificazione
-    WHERE Certificato_Richiesto = 'DOCG' && Esito = 1;
+CREATE OR REPLACE VIEW VINO AS
+    SELECT Tipo_Vino.Nome_Vino,Tipo_Vino.Colore,Tipo_Vino.Gradazione,Tipo_Vino.Temp_Servizio
+    FROM Tipo_Vino
+    GROUP BY Tipo_Vino.Nome_Vino;
 
 CREATE OR REPLACE VIEW N_Ordini_Clienti AS
-    SELECT Carrello.Ragione_Sociale, COUNT(Carrello.Ragione_Sociale) AS Numero_Ordini
+    SELECT Carrello.Ragione_Sociale,Carrello.Nome_Cl, COUNT(Carrello.Ragione_Sociale) AS Numero_Ordini
     FROM Carrello
     GROUP BY Carrello.Ragione_Sociale;
 
-CREATE OR REPLACE VIEW Vini_Per_Specie AS 
-    SELECT Nome_vino, Specie
-    FROM Composizione_Vino 
-    GROUP BY Specie;
+CREATE OR REPLACE VIEW CONF AS
+    SELECT Confezione.Num_Conf,Confezione.Nome_Vino
+    FROM Confezione
+    GROUP BY Confezione.Num_Conf;
+
+CREATE OR REPLACE VIEW ORDINI_CL AS
+    SELECT Carrello.Nome_Cl,Carrello.Ragione_Sociale,Carrello.Nome_Vino
+    FROM Carrello
+    GROUP BY Carrello.Nome_Cl;
+
+    #-------------VISTE CHE SERVONO------------#
+    #CARRELLO CLIENTE
+    #TIPO_VINO CLIENTE
+    #CONFEZIONE CLIENTE
+    #
