0e6bd9f9313fc3a9cb3f9117d83db0ad1b0ab5bc
Pasquale Ruotolo <77306367+Just-Ipavon@users.noreply.github.com>
2022-07-20 10:54:21 +0200

    viste

--- Codice/Viste.sql
+++ Codice/Viste.sql
@@ -10,8 +10,7 @@ CREATE OR REPLACE VIEW Vini_DOP AS
     
 CREATE OR REPLACE VIEW VINO AS
     SELECT Tipo_Vino.Nome_Vino,Tipo_Vino.Colore,Tipo_Vino.Gradazione,Tipo_Vino.Temp_Servizio
-    FROM Tipo_Vino
-    GROUP BY Tipo_Vino.Nome_Vino;
+    FROM Tipo_Vino;
 
 CREATE OR REPLACE VIEW Vini_DOCG AS
     SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
@@ -19,19 +18,14 @@ CREATE OR REPLACE VIEW Vini_DOCG AS
     WHERE Certificato_Richiesto = 'DOCG' AND Esito = 1;
 
 CREATE OR REPLACE VIEW N_Ordini_Clienti AS
-    SELECT Carrello.Ragione_Sociale,Carrello.Nome_Cl, COUNT(Carrello.Ragione_Sociale) AS Numero_Ordini
+    SELECT Carrello.Ragione_Sociale, COUNT(Carrello.Ragione_Sociale) AS Numero_Ordini
     FROM Carrello
     GROUP BY Carrello.Ragione_Sociale;
 
 CREATE OR REPLACE VIEW CONF AS
     SELECT Confezione.Num_Conf,Confezione.Nome_Vino
     FROM Confezione
-    GROUP BY Confezione.Num_Conf;
-
-CREATE OR REPLACE VIEW ORDINI_CL AS
-    SELECT Carrello.Nome_Cl,Carrello.Ragione_Sociale,Carrello.Nome_Vino
-    FROM Carrello
-    GROUP BY Carrello.Nome_Cl;
+    GROUP BY Confezione.Num_Conf, Confezione.Nome_Vino;
 
     #-------------VISTE CHE SERVONO------------#
     #CARRELLO CLIENTE fatta
