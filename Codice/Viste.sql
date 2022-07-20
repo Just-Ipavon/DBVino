b02242f6f34a43f8a0bd43fc9dff4666056142a4
Pasquale Ruotolo <77306367+Just-Ipavon@users.noreply.github.com>
2022-07-20 10:30:35 +0200

    viste

--- Codice/Viste.sql
+++ Codice/Viste.sql
@@ -1,31 +1,23 @@
-<<<<<<< HEAD
 CREATE OR REPLACE VIEW Vini_DOC AS
     SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
     FROM Controllo_Certificazione
     WHERE Certificato_Richiesto = 'DOC' AND Esito = 1;
-=======
->>>>>>> 8d8b34b383e5673acb9cba3a1cc130b0928f965a
 
-<<<<<<< HEAD
 CREATE OR REPLACE VIEW Vini_DOP AS
     SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
     FROM Controllo_Certificazione
     WHERE Certificato_Richiesto = 'DOP' AND Esito = 1;
-=======
+    
 CREATE OR REPLACE VIEW VINO AS
     SELECT Tipo_Vino.Nome_Vino,Tipo_Vino.Colore,Tipo_Vino.Gradazione,Tipo_Vino.Temp_Servizio
     FROM Tipo_Vino
     GROUP BY Tipo_Vino.Nome_Vino;
->>>>>>> 8d8b34b383e5673acb9cba3a1cc130b0928f965a
 
-<<<<<<< HEAD
 CREATE OR REPLACE VIEW Vini_DOCG AS
     SELECT Nome_Vino, Data_Certif, Certificato_Richiesto
     FROM Controllo_Certificazione
     WHERE Certificato_Richiesto = 'DOCG' AND Esito = 1;
 
-=======
->>>>>>> 8d8b34b383e5673acb9cba3a1cc130b0928f965a
 CREATE OR REPLACE VIEW N_Ordini_Clienti AS
     SELECT Carrello.Ragione_Sociale,Carrello.Nome_Cl, COUNT(Carrello.Ragione_Sociale) AS Numero_Ordini
     FROM Carrello
